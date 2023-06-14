f87a0a889e27b4746a7327432ca4e3c97bec7688
ksenkl <klepa@vivaldi.net>
2023-06-15 02:55:43 +0300

    SnakeClass

---
+++ src/main/java/ru/ku/jungle/model/Snake.java
@@ -0,0 +1,24 @@
+package ru.ku.jungle.model;
+
+public class Snake {
+    private int health = 100;
+    private int energy = 100;
+    private final double preyEnergyFactor = 3.1;
+
+    public void setHealth (int health) {
+        this.health = health;
+    }
+    public int getHealth(){
+        return this.health;
+    }
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+    public int getEnergy() {
+        return this.energy;
+    }
+    public double getPreyEnergyFactor() {
+        return this.preyEnergyFactor;
+    }
+
+}
