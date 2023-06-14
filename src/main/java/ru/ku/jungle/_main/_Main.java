d5c2a9ed565c1d035464262bc3a82233ac3e84f0
ksenkl <klepa@vivaldi.net>
2023-06-15 02:58:12 +0300

    commit _Main

---
+++ src/main/java/ru/ku/jungle/_main/_Main.java
@@ -0,0 +1,14 @@
+package ru.ku.jungle._main;
+import ru.ku.jungle.model.Snake;
+import ru.ku.jungle.util.EventManager;
+
+public class _Main {
+    public static void main(String[] args) {
+        Snake snake = new Snake();
+        EventManager eventManager = new EventManager();
+        eventManager.startEvents(snake);
+
+
+
+    }
+}
